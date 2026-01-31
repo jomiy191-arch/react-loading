4c4a2844afa3c2c8c8a576aeee7c8e08883dd1e9
Sizning GitHub username <sizning_email@github.com>
2026-01-31 13:40:28 +0500

    Some changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import People from './components/People/People';
 import Testimonials from './components/Testimonials/Testimonials';
 import Get from './components/Get/Get';
 import Enjoy from './components/Enjoy/Enjoy';
-// import Loading from './components/Loading/Loading'; // Loading import
+
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import './App.css';
@@ -16,14 +16,14 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // AOS animatsiyalarini init qilamiz
+
     AOS.init({
       duration: 1000,
       easing: 'ease-in-out',
       once: true,
     });
 
-    // Loadingni 2 sekund ko‘rsatish
+    
     const timer = setTimeout(() => {
       setLoading(false);
     }, 2000);
@@ -33,10 +33,7 @@ function App() {
 
   return (
     <>
-      {/* {loading ? (
-        <Loading />
-      ) : (
-        <div className="App"> */}
+
           <Header />
           <Hero />
           <Catalog />
@@ -45,8 +42,7 @@ function App() {
           <People />
           <Testimonials />
           <Get />
-        {/* </div> */}
-      {/* )} */}
+
     </>
   );
 }
